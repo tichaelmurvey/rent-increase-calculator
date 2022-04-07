e172be725ebe7cd3fd2943f81cc4b7e4b93193f5
queenbeeslick <michaelturvey97@gmail.com>
2022-04-07 15:25:50 +1000

    fixed stuff

--- test.js
+++ test.js
@@ -21,11 +21,21 @@ fetch(request).then(
             qstart_cpi = results_array[0];
             qend_cpi = results[results_array.length -1];
             cpi_change = 100*(qend_cpi-qstart_cpi)/qstart_cpi;
+            allowable_increase = cpi_change*1.1
+            current_rent = $("#current-rent");
+            max_increase = current_rent*allowable_increase;
+            max_new_rent = current_rent + max_increase;
             cpi_change = Math.round(cpi_change*100)/100;
+            allowable_increase = Math.round(allowable_increase*100)/100;
+            max_increase = Math.round(max_increase);
+            max_new_rent = Math.round(max_new_rent);
             $("#result").empty();
             $("#result").append("<p>" + 'Starting quarter CPI: ' + qstart_cpi + "</p>");
             $("#result").append("<p>" + 'Ending quarter CPI: ' + qend_cpi + "</p>");
             $("#result").append("<p>" + 'Change in CPI: ' + cpi_change + "%</p>");
+            $("#result").append("<p>" + 'Allowable increase: ' + allowable_increase + "%</p>");
+            $("#result").append("<p>" + 'Allowable increase: $' + allowable_increase + "%</p>");
+            $("#result").append("<p>" + 'Maximum new rent: ' + allowable_increase + "%</p>");
         }
     )
 }
